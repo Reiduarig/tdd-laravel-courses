005725ca606d6dd9b8a3a77deb64c7ac70b8976a
Reiduarig <giraudier82@gmail.com>
2025-11-11 10:46:06 +0100

    Modificar nombre del archivo test en feature para reutilizar en pruebas de pagina

---
+++ tests/Feature/PageResponseTest.php
@@ -0,0 +1,10 @@
+<?php
+
+use function Pest\Laravel\get;
+
+test('the application returns a successful response', function () {
+    
+    // Act & Assert, make a GET request to the home page and assert a 200 OK response
+    get(route('home'))
+        ->assertOk();
+});
